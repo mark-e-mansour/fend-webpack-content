fefa19b2e47bc1e0467d8771c022b4789d6efba8
MEM <m.emil@binalogic.com>
2021-03-07 22:37:22 +0200

    current state

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,25 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/client/index.js',
 }
+
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              },
+              {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
+    ]
+}
